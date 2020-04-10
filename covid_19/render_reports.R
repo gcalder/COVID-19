2901b96ae693e7ecc6d81ca7e96920d27bb07f01
smazeri <stellamazeri@gmail.com>
2020-04-10 16:27:06 +0100

    more sensible rmd names

--- covid_19/render_reports.R
+++ covid_19/render_reports.R
@@ -1,9 +1,9 @@
 ### render reports
 library(rmarkdown)
 library(stringr)
-render("COVID-19_Scotland_Daily_Update_2020_04_.Rmd", 
+render("COVID-19_Scotland_Daily_Update.Rmd", 
        output_file = str_glue("COVID-19_Scotland_Daily_Update_", {str_replace_all(Sys.Date(), pattern = "-", "_")}, ".html"))
 
-render("COVID-19_Scotland_Daily_Update_2020_04_pdf.Rmd", 
+render("COVID-19_Scotland_Daily_Update_pdf.Rmd", 
        output_file = str_glue("COVID-19_Scotland_Daily_Update_", {str_replace_all(Sys.Date(), pattern = "-", "_")}, ".pdf"))
 
